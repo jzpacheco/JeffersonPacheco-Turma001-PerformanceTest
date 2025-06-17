e21aa0385e4ce987dc827c1b09863543f40d1935
jzpacheco <jeffersonpacheco@hotmail.com>
2025-06-17 20:19:12 -0300

    git init

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -9,14 +9,22 @@ export const RateContentOK = new Rate('content_OK');
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.30'],
-    get_contacts: ['p(99)<500'],
-    content_OK: ['rate>0.95']
+    http_req_duration: ['p(95) < 5700'],
+    http_req_failed: ['rate < 0.12'],  
   },
   stages: [
-    { duration: '10s', target: 5 },
-    { duration: '20s', target: 15 }
-  ]
+    { duration: '30s', target: 50 },
+    { duration: '10s', target: 40 },
+    { duration: '60s', target: 80 },
+    { duration: '20s', target: 70 },
+    { duration: '40s', target: 110 },
+    { duration: '20s', target: 100 },
+    { duration: '40s', target: 130 },
+    { duration: '30s', target: 140 },
+    { duration: '20s', target: 230 },  
+    { duration: '30s', target: 300 },
+  ],
+  maxDuration: '300s',  
 };
 
 export function handleSummary(data) {
@@ -27,17 +35,17 @@ export function handleSummary(data) {
 }
 
 export default function () {
-  const baseUrl = 'https://test.k6.io/';
+  const baseUrl = 'https://dog.ceo/api/breeds/image/random';
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+  // const params = {
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   }
+  // };
 
   const OK = 200;
 
-  const res = http.get(`${baseUrl}`, params);
+  const res = http.get(`${baseUrl}`); //,params
 
   getContactsDuration.add(res.timings.duration);
 
